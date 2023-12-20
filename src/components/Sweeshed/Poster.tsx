7694b4c0bd0ed9351283fc21eba41a499120addd
waqas7658 <wa266867@gmail.com>
2023-12-20 16:04:46 +0500

    ss

--- src/components/Sweeshed/Poster.tsx
+++ src/components/Sweeshed/Poster.tsx
@@ -70,8 +70,8 @@ export default function Poster() {
                         <img
                           className="my-5 h-15 w-40 object-cover"
                           // src={item?.event_Picture}
-                          // src={`${BASEURL}/${image}`}
-                          src={`${BASEURL}/${image.replace(/\/+/g, '/')}`}
+                          src={`${BASEURL}/${image}`}
+                          // src={`${BASEURL}/${image.replace(/\/+/g, '/')}`}
 
                           alt="Event"
                         />
