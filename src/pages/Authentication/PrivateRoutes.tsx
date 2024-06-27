8adab1a55d154e398c40b5c7144f3c94dc29ae2c
SalluMia <sallumia9090@gmail.com>
2024-06-27 12:54:28 +0500

    artifacts renew data

--- src/pages/Authentication/PrivateRoutes.tsx
+++ src/pages/Authentication/PrivateRoutes.tsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRoutes = () => {
   const isAuthenticated = JSON.parse(localStorage.getItem('login'));
-  return isAuthenticated ? <Outlet /> : <Navigate to="/auth/signin" />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
 
 export default PrivateRoutes;
